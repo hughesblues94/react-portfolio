85c57cb9898d2e336a76ce8d4fe21922126c2403
hughesblues94 <hughesblues94@gmail.com>
2023-02-24 17:06:22 +0000

    added scrolling to right hand side of page

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -13,7 +13,19 @@ const Bio = () => {
         Hello! I am a software developer who has recently finished the Command
         Shift bootcamp. I am currently open to work!
       </p>
-      <Button buttonName={"contact"}></Button>
+
+      <h1 className="projects">Projects</h1>
+      <h2>Here are some examples of some projects:</h2>
+      <p>example</p>
+      <p>example</p>
+      <p>example</p>
+      <p>example</p>
+      <p>example</p>
+      <p>example</p>
+      <p>example</p>
+      <p>example</p>
+      <p>example</p>
+      <p>example</p>
     </div>
   );
 };
