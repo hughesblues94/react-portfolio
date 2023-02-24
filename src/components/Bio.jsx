86b7dda58cc62f74fa1f594432764b7acd318f4a
hughesblues94 <hughesblues94@gmail.com>
2023-02-24 18:17:31 +0000

    removed fa icons

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,7 +1,5 @@
 import "./Bio.css";
 import Button from "./Button.jsx";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { falinkedin } from "@fortawesome/free-solid-svg-icons";
 
 const Bio = () => {
   return (
@@ -15,6 +13,7 @@ const Bio = () => {
         Hello! I am a software developer who has recently finished the Command
         Shift bootcamp. I am currently open to work!
       </p>
+      <hr></hr>
       <h1 id="projects" className="projects">
         Projects
       </h1>
@@ -29,9 +28,19 @@ const Bio = () => {
       <p>example</p>
       <p>example</p>
       <p>example</p>
+      <hr></hr>
+
+      <h1 id="skills">Skills</h1>
+
+      <h2>Some technologies I have worked with:</h2>
+      <p>Javascript</p>
+      <p>HTML</p>
+      <p>CSS</p>
+      <p>React</p>
+      <p>Express</p>
+
       <h1 id="contact">Contact</h1>
       <h2>Find me on socials:</h2>
-      <FontAwesomeIcon icon={falinkedin} />
     </div>
   );
 };
